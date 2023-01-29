6b6213013d09c2727d2247b19167aaebaf7544ad
gkafkias <giwrgoskafkias96@gmail.com>
2023-01-29 10:08:43 +0200

    Added Highcharts package

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,28 @@ import { Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "./slices/testSlice";
 import { RootState } from "./store/store";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 function App() {
   const dispatch = useDispatch();
   const { value } = useSelector((state: RootState) => state.test);
 
+  const options = {
+    title: {
+      text: "My chart",
+    },
+    series: [
+      {
+        data: [1, 2, 3],
+      },
+    ],
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <HighchartsReact highcharts={Highcharts} options={options} />
         <Button type="primary" onClick={() => dispatch(increment())}>
           Testing
         </Button>
